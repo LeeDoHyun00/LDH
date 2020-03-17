2a25413b674f135320e8c0010201f34f6b2b5f77
LeeDoHyun00 <62238799+LeeDoHyun00@users.noreply.github.com>
2020-03-17 22:58:08 +0900

    Create 2445.c
    별찍기 8

---
+++ 2445.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+    int x;
+    scanf("%d",&x);
+    
+    for(int z=1;z<x+1;z++){
+        for(int a=0;a<z;a++)
+        printf("*");
+        for(int a=0;a<x-z;a++)
+        printf(" ");
+        for(int a=0;a<x-z;a++)
+        printf(" ");
+        for(int a=0;a<z;a++)
+        printf("*");
+        printf("\n");
+    }
+    
+    for(int z=1;z<x;z++){
+        for(int a=0;a<x-z;a++)
+        printf("*");
+        for(int a=0;a<z;a++)
+        printf(" ");
+        for(int a=0;a<z;a++)
+        printf(" ");
+        for(int a=0;a<x-z;a++)
+        printf("*");
+        printf("\n");
+    }
+    
+    return 0;
+}
