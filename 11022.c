aa98f5b7ba130f730e6cba8d07c9ba9e3a0cec4e
LeeDoHyun00 <62238799+LeeDoHyun00@users.noreply.github.com>
2020-03-30 18:12:41 +0900

    Create 11022.c
    for문 8

---
+++ 11022.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int x,y,r=0;
+    scanf("%d",&x);
+    for(int z=1;z<x+1;z++){
+        scanf("%d %d",&y,&r);
+        printf("Case #%d: %d + %d = %d\n",z,y,r,y+r);
+    }
+    return 0;
+}
